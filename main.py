b13b8dbefb2e9e6db5903192dec3ae13ba55045b
kassyiemobi <omeh.kasie@gmail.com>
2020-03-06 20:59:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#defining a new function and adding parametres.
+def multiplayer(name,country):
+  #using the formated string.
+  print(f'hello, {name} {country}')
+multiplayer('kasie', 'Nigeria')
+multiplayer('kelvin', 'botswana')
+multiplayer('rita', 'algeria')
